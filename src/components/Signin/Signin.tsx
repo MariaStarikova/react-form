55bb13347bb7cef826b233e1a6403b0137099801
Starikova Maria <maria050200@gmail.com>
2025-05-23 16:59:07 +0500

    feat: add registration and login forms

---
+++ src/components/Signin/Signin.tsx
@@ -0,0 +1,58 @@
+import { Form, type FormField } from '../Form/Form';
+import type { ValidationRules } from '../../hooks/useFormValidation.tsx';
+import './Signin.scss';
+
+const mockSigninInputs: FormField[] = [
+  {
+    name: 'email',
+    variant: 'filled',
+    label: 'Email',
+    description: 'Введите email',
+    type: 'email',
+    placeholder: 'email',
+    required: true,
+    size: 'md',
+    radius: 'md'
+  },
+  {
+    name: 'password',
+    variant: 'filled',
+    label: 'Password',
+    description: 'Введите пароль',
+    type: 'password',
+    placeholder: 'password',
+    required: true,
+    size: 'md',
+    radius: 'md'
+  }
+];
+
+const signinValidationRules: ValidationRules = {
+  email: {
+    required: true,
+    email: true
+  },
+  password: {
+    required: true,
+    minLength: 6
+  }
+};
+
+export function Signin() {
+  const handleSigninSubmit = (data: Record<string, string>) => {
+    console.log('Форма входа отправлена! Данные для вхада:', data);
+  };
+
+  return (
+    <div className="signin">
+      <Form
+        title="Signin form"
+        fields={mockSigninInputs}
+        validationRules={signinValidationRules}
+        onSubmit={handleSigninSubmit}
+        submitButtonText="Войти"
+        className="signin__form"
+      />
+    </div>
+  );
+}
---
+++ src/icons/Icon.tsx
@@ -0,0 +1,18 @@
+export function Icon({ size = 16, color = 'currentColor' }: { size?: number; color?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="12" cy="12" r="4" />
+      <path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94" />
+    </svg>
+  );
+}
